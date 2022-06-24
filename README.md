a0110b963fefdedc6b82ef360e2c7d0b6c221b9a
wangshengqiang <527538586@qq.com>
2022-06-24 15:25:06 +0800

    [update]阿拉伯语适配

--- README.md
+++ README.md
@@ -10,9 +10,13 @@ RecyclerView重新封装,实现通用的数据适配器，上拉加载，下拉�
 
 ## Step 2. Add the dependency
 
-* api 'com.github.shengwang520:SRecyclerView:1.0.19'
+* api 'com.github.shengwang520:SRecyclerView:1.0.20'
 
 ## Changelog
+
+### Version:1.0.20
+
+* 新增卡片分割线适配阿拉伯语
 
 ### Version:1.0.19
 
--- srecyclerview/src/main/java/com/sheng/wang/srecyclerview/decoration/CardDecoration.kt
+++ srecyclerview/src/main/java/com/sheng/wang/srecyclerview/decoration/CardDecoration.kt
@@ -1,8 +1,11 @@
 package com.sheng.wang.srecyclerview.decoration
 
 import android.graphics.Rect
+import android.util.LayoutDirection
 import android.view.View
+import androidx.core.text.TextUtilsCompat
 import androidx.recyclerview.widget.RecyclerView
+import java.util.*
 
 /**
  * @param mPageMargin 卡片间距
@@ -19,16 +22,31 @@ class CardDecoration @JvmOverloads constructor(
     override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
         val position = parent.getChildAdapterPosition(view); //获得当前item的position
         val itemCount = parent.adapter?.itemCount ?: 0 //获得item的数量
-        val leftMargin = if (position == 0) {
-            mLeftPageVisibleWidth
+
+        var leftMargin = 0
+        var rightMargin = 0
+        if (position == 0) {
+            if (TextUtilsCompat.getLayoutDirectionFromLocale(Locale.getDefault()) == LayoutDirection.RTL) {//界面反转
+                leftMargin = mPageMargin
+                rightMargin = mLeftPageVisibleWidth
+            } else {
+                leftMargin = mLeftPageVisibleWidth
+                rightMargin = mPageMargin
+            }
+        } else if (position == itemCount - 1) {
+            if (TextUtilsCompat.getLayoutDirectionFromLocale(Locale.getDefault()) == LayoutDirection.RTL) {//界面反转
+                leftMargin = mLeftPageVisibleWidth
+                rightMargin = mPageMargin
+            } else {
+                leftMargin = mPageMargin
+                rightMargin = mLeftPageVisibleWidth
+            }
         } else {
-            mPageMargin
+            leftMargin = mPageMargin
+            rightMargin = mPageMargin
         }
-        val rightMargin = if (position == itemCount - 1) {
-            mLeftPageVisibleWidth
-        } else {
-            mPageMargin
-        }
+
+
         val lp = view.layoutParams as RecyclerView.LayoutParams
         lp.width = viewWidth
         lp.setMargins(leftMargin, topMargin, rightMargin, bottomMargin)
